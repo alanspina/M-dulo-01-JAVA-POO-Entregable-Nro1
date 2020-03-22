f709a5b0ea4caf56dfb1142e7b702b586bae7ecb
alan <alanspina@gmail.com>
2020-03-22 19:47:54 -0300

    Parte G
    1. Realizar un diagrama de clase que modele a la clase DigitalHouseManager.
    En principio, DigitalHouseManager tiene una lista de alumnos (List <Alumno>), una lista de
    profesores (List <Profesor>), una lista de cursos (List <Curso>) y una lista de inscripciones
    (List <Inscripcion>).
    2. Implementar la clase creando los atributos necesarios.

---
+++ src/com/company/DigitalHouseManager.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.List;
+
+public class DigitalHouseManager {
+    List<Profesor> listaProfesor;
+    List<Alumno> listaAlumno;
+    List<Curso> listaCurso;
+    List<Inscripcion> listaInscripcion;
+
+    
+}
