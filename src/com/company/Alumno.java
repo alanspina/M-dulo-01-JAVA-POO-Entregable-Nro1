78434bfd412ce02bf9ce156f3dac3227c604fce7
alan <alanspina@gmail.com>
2020-03-22 16:21:43 -0300

    Parte A
    1. Realizar un diagrama de clase que modele a la clase Alumno.
    En principio, un alumno posee un nombre (String), un apellido (String) y un código de alumno
    (Integer).
    2. Implementar la clase creando los atributos necesarios.
    3. Crear un constructor para el alumno que tome por parámetro un nombre, un apellido y un código
    de alumno.
    4. Un alumno es igual a otro si sus códigos de alumno son iguales

---
+++ src/com/company/Alumno.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Alumno {
+    private String nombre;
+    private String apellido;
+    private Integer codigoDeAlumno;
+
+    public Alumno(String nombre, String apellido, Integer codigoDeAlumno) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.codigoDeAlumno = codigoDeAlumno;
+    }
+
+    //Crear un constructor para el alumno que tome por parámetro un nombre, un apellido y un código
+    //de alumno.
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(codigoDeAlumno, alumno.codigoDeAlumno);
+    }
+
+
+    // Un alumno es igual a otro si sus códigos de alumno son iguales
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigoDeAlumno);
+    }
+}
